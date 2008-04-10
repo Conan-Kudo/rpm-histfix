rpm-software-management/rpm

commit 6c4b0cbd93709a0fa18af1ef07af44da585bb5f6

Rip the last remaining disk/file URL stuff in processSourceFiles()

build/files.c
@@ -1767,7 +1767,7 @@ static rpmRC processPackageFiles(rpmSpec spec, Package pkg,
 	FILE * f;
 	FD_t fd;
 
-	/* XXX W2DO? */
+	/* XXX W2DO? urlPath might be useful here. */
 	if (*pkg->fileFile == '/') {
 	    ffn = rpmGetPath(pkg->fileFile, NULL);
 	} else {
@@ -2146,38 +2146,34 @@ int processSourceFiles(rpmSpec spec)
     /* The first source file is the spec file */
     x = 0;
     for (fp = files; *fp != NULL; fp++) {
-	const char * diskURL, *diskPath;
+	const char *diskPath;
 	FileListRec flp;
 
-	diskURL = *fp;
-	SKIPSPACE(diskURL);
-	if (! *diskURL)
+	diskPath = *fp;
+	SKIPSPACE(diskPath);
+	if (! *diskPath)
 	    continue;
 
 	flp = &fl.fileList[x];
 
 	flp->flags = isSpec ? RPMFILE_SPECFILE : 0;
 	/* files with leading ! are no source files */
-	if (*diskURL == '!') {
+	if (*diskPath == '!') {
 	    flp->flags |= RPMFILE_GHOST;
-	    diskURL++;
+	    diskPath++;
 	}
 
-	(void) urlPath(diskURL, &diskPath);
-
-	flp->diskPath = xstrdup(diskURL);
+	flp->diskPath = xstrdup(diskPath);
 	diskPath = strrchr(diskPath, '/');
 	if (diskPath)
 	    diskPath++;
-	else
-	    diskPath = diskURL;
 
 	flp->cpioPath = xstrdup(diskPath);
 	flp->verifyFlags = RPMVERIFY_ALL;
 
-	if (stat(diskURL, &flp->fl_st)) {
+	if (stat(diskPath, &flp->fl_st)) {
 	    rpmlog(RPMLOG_ERR, _("Bad file: %s: %s\n"),
-		diskURL, strerror(errno));
+		diskPath, strerror(errno));
 	    fl.processingFailed = 1;
 	}
 
@@ -2200,7 +2196,7 @@ int processSourceFiles(rpmSpec spec)
 	fl.totalFileSize += flp->fl_size;
 	
 	if (! (flp->uname && flp->gname)) {
-	    rpmlog(RPMLOG_ERR, _("Bad owner/group: %s\n"), diskURL);
+	    rpmlog(RPMLOG_ERR, _("Bad owner/group: %s\n"), diskPath);
 	    fl.processingFailed = 1;
 	}
 

Conan-Kudo/rpm-histfix

commit 0297c3450a9ade553a724749411c527ef3ce38ea

Unbreak srpm generation broken by 6c4b0cbd93709a0fa18af1ef07af44da585bb5f6 - cpio path in source packages is the basename of the file, make this   obvious by using basename() instead of pointer games

build/files.c
@@ -2147,6 +2147,7 @@ int processSourceFiles(rpmSpec spec)
     x = 0;
     for (fp = files; *fp != NULL; fp++) {
 	const char *diskPath;
+	char *tmp;
 	FileListRec flp;
 
 	diskPath = *fp;
@@ -2163,13 +2164,11 @@ int processSourceFiles(rpmSpec spec)
 	    diskPath++;
 	}
 
+	tmp = xstrdup(diskPath); /* basename() might modify */
 	flp->diskPath = xstrdup(diskPath);
-	diskPath = strrchr(diskPath, '/');
-	if (diskPath)
-	    diskPath++;
-
-	flp->cpioPath = xstrdup(diskPath);
+	flp->cpioPath = xstrdup(basename(tmp));
 	flp->verifyFlags = RPMVERIFY_ALL;
+	free(tmp);
 
 	if (stat(diskPath, &flp->fl_st)) {
 	    rpmlog(RPMLOG_ERR, _("Bad file: %s: %s\n"),