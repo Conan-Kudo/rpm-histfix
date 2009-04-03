rpm-software-management/rpm

commit 8d6c4b8c95b59f5a71d90c582c2e98f5c7ed7b9d

Add + use internal helper function for setting rpmfi file states - instead of directly accessing the array from fsm, use a helper function   which lazily allocates the state array as needed and sets state - also fixes the silly case of non-installed packages showing their files   as "normal", ie installed (now it shows "no state" as it's not relevant)

lib/fsm.c
@@ -643,22 +643,23 @@ static int fsmMapPath(FSM_t fsm)
 	    break;
 	case FA_COPYIN:
 	case FA_CREATE:
-assert(rpmteType(fi->te) == TR_ADDED);
+	    if (rpmteType(fi->te) == TR_ADDED)
+		rpmfiSetFState(fi, i, RPMFILE_STATE_NORMAL);
 	    break;
 
 	case FA_SKIPNSTATE:
-	    if (fi->fstates && rpmteType(fi->te) == TR_ADDED)
-		fi->fstates[i] = RPMFILE_STATE_NOTINSTALLED;
+	    if (rpmteType(fi->te) == TR_ADDED)
+		rpmfiSetFState(fi, i, RPMFILE_STATE_NOTINSTALLED);
 	    break;
 
 	case FA_SKIPNETSHARED:
-	    if (fi->fstates && rpmteType(fi->te) == TR_ADDED)
-		fi->fstates[i] = RPMFILE_STATE_NETSHARED;
+	    if (rpmteType(fi->te) == TR_ADDED)
+		rpmfiSetFState(fi, i, RPMFILE_STATE_NETSHARED);
 	    break;
 
 	case FA_SKIPCOLOR:
-	    if (fi->fstates && rpmteType(fi->te) == TR_ADDED)
-		fi->fstates[i] = RPMFILE_STATE_WRONGCOLOR;
+	    if (rpmteType(fi->te) == TR_ADDED)
+		rpmfiSetFState(fi, i, RPMFILE_STATE_WRONGCOLOR);
 	    break;
 
 	case FA_BACKUP:

lib/rpmfi.c
@@ -1291,13 +1291,12 @@ rpmfi rpmfiNew(const rpmts ts, Header h, rpmTag tagN, rpmfiFlags flags)
 	_hgfi(h, RPMTAG_FILEDEPENDSN, &td, scareFlags, fi->fddictn);
     }
 
-    /* XXX States not needed by TR_REMOVED */
+    /*
+     * For installed packages, get the states here. For to-be-installed
+     * packages fi->fstates is lazily created through rpmfiSetFState().
+     * XXX file states not needed at all by TR_REMOVED.
+     */
     _hgfi(h, RPMTAG_FILESTATES, &td, defFlags, fi->fstates);
-    if (fi->fstates == NULL) {
-	fi->fstates = xmalloc(sizeof(*fi->fstates) * fi->fc);
-	/* XXX means we show state "normal" when package not even installed */
-	memset(fi->fstates, RPMFILE_STATE_NORMAL, fi->fc);
-    }
 
     _hgfi(h, RPMTAG_FILECAPS, &td, defFlags, fi->fcaps);
 
@@ -1452,3 +1451,13 @@ rpmfi rpmfiUpdateState(rpmfi fi, rpmts ts, rpmte p)
     return fi;
 }
 
+void rpmfiSetFState(rpmfi fi, int ix, rpmfileState state)
+{
+    if (fi != NULL && ix >= 0 && ix < fi->fc) {
+	if (fi->fstates == NULL) {
+	    fi->fstates = xmalloc(sizeof(*fi->fstates) * fi->fc);
+	    memset(fi->fstates, RPMFILE_STATE_MISSING, fi->fc);
+	}
+	fi->fstates[ix] = state;
+    }
+}

lib/rpmfi_internal.h
@@ -115,5 +115,8 @@ int nrefs;		/*!< Reference count. */
 RPM_GNUC_INTERNAL
 rpmfi rpmfiUpdateState(rpmfi fi, rpmts ts, rpmte p);
 
+RPM_GNUC_INTERNAL
+void rpmfiSetFState(rpmfi fi, int ix, rpmfileState state);
+
 #endif	/* _RPMFI_INTERNAL_H */
 

Conan-Kudo/rpm-histfix

commit 04ef5dfa7959654b7bcdb93f856eaa7d3c736340

Make sure installed files have state (rhbz#492947) - rpmfsSetState() doesn't get called for skipped files like %ghost and   %config(noreplace), causing incorrect file state ("no state") getting   recorded in rpmdb, leading to inapproriate removal/rename on erase, ick - For TR_ADDED, always default file states to RPMFILE_STATE_NORMAL, fsm   changes it as necessary for skipped colors and such. Lazy alloc on   rpmfsSetState() is not correct as rpmfsSetState() might not get called   at all. - originally broken by commit 8d6c4b8c95b59f5a71d90c582c2e98f5c7ed7b9d

lib/fsm.c
@@ -663,8 +663,6 @@ static int fsmMapPath(FSM_t fsm)
 	    break;
 	case FA_COPYIN:
 	case FA_CREATE:
-	    if (rpmteType(te) == TR_ADDED)
-		rpmfsSetState(fs, i, RPMFILE_STATE_NORMAL);
 	    break;
 
 	case FA_SKIPNSTATE:

lib/rpmte.c
@@ -285,7 +285,7 @@ static void addTE(rpmts ts, rpmte p, Header h,
 	struct rpmtd_s bnames;
 	headerGet(h, RPMTAG_BASENAMES, &bnames, HEADERGET_MINMEM);
 
-	p->fs = rpmfsNew(rpmtdCount(&bnames));
+	p->fs = rpmfsNew(rpmtdCount(&bnames), p->type);
 
 	rpmtdFreeData(&bnames);
     }
@@ -896,11 +896,15 @@ rpmfs rpmteGetFileStates(rpmte te) {
     return te->fs;
 }
 
-rpmfs rpmfsNew(unsigned int fc) {
+rpmfs rpmfsNew(unsigned int fc, rpmElementType type) {
     rpmfs fs = xmalloc(sizeof(*fs));
     fs->fc = fc;
     fs->replaced = NULL;
     fs->states = NULL;
+    if (type == TR_ADDED) {
+	fs->states = xmalloc(sizeof(*fs->states) * fs->fc);
+	memset(fs->states, RPMFILE_STATE_NORMAL, fs->fc);
+    }
     fs->actions = xmalloc(fc * sizeof(*fs->actions));
     memset(fs->actions, FA_UNKNOWN, fc * sizeof(*fs->actions));
     fs->numReplaced = fs->allocatedReplaced = 0;
@@ -958,10 +962,6 @@ sharedFileInfo rpmfsNextReplaced(rpmfs fs , sharedFileInfo replaced)
 void rpmfsSetState(rpmfs fs, unsigned int ix, rpmfileState state)
 {
     assert(ix < fs->fc);
-    if (fs->states == NULL) {
-	fs->states = xmalloc(sizeof(*fs->states) * fs->fc);
-	memset(fs->states, RPMFILE_STATE_MISSING, fs->fc);
-    }
     fs->states[ix] = state;
 }
 

lib/rpmte_internal.h
@@ -81,7 +81,7 @@ int rpmteHaveTransScript(rpmte te, rpmTag tag);
 rpmfs rpmteGetFileStates(rpmte te);
 
 RPM_GNUC_INTERNAL
-rpmfs rpmfsNew(unsigned int fc);
+rpmfs rpmfsNew(unsigned int fc, rpmElementType type);
 
 RPM_GNUC_INTERNAL
 rpmfs rpmfsFree(rpmfs fs);