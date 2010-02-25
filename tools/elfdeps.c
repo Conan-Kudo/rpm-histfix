rpm-software-management/rpm

commit 87c237bebfc792a8b439fa2c5a2b78328b139d50

Split elf dependency extraction out of librpmbuild to external helper - Elf dependency extraction code code lifted from rpmfcELF() and refactored   to saner pieces. Having it in separate executable also frees librpmbuild   of libelf dependency, clean up the unnecessary linkage etc from   autofoo - This lets internal dependency generator for elf files be   overridden without losing file coloring (which is required for   correct multilib handling). It also permits non-native elf files   (eg when cross-building) to be handled by providing a custom   elf dependency helper - On the flip side, this inevitably slows down builds somewhat as   two fork-exec's are needed for every elf file, but unlike invoking   something like the python interpreter, this is a slim helper... - All dependency extractors of the internal dependency generator are now   external helpers (how twisted is that, huh? :) and thus can be customized   and filtered through %__foo_provides|requires macros

tools/elfdeps.c
@@ -1,0 +1,296 @@
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <error.h>
+#include <errno.h>
+#include <popt.h>
+#include <gelf.h>
+
+#include <rpm/rpmstring.h>
+#include <rpm/argv.h>
+
+int filter_private = 0;
+
+typedef struct elfInfo_s {
+    Elf *elf;
+
+    int isDSO;
+    int isElf64;		/* is 64bit marker needed in dependencies */
+    int isExec;			/* requires are only added to executables */
+    int gotDEBUG;
+    int gotHASH;
+    int gotGNUHASH;
+    int gotSONAME;
+
+    ARGV_t requires;
+    ARGV_t provides;
+} elfInfo;
+
+static int skipPrivate(const char *s)
+{ 
+    return (filter_private && rstreq(s, "GLIBC_PRIVATE"));
+}
+
+static void processVerDef(Elf_Scn *scn, GElf_Shdr *shdr, elfInfo *ei)
+{
+    Elf_Data *data;
+    unsigned int offset, auxoffset;
+    char *soname = NULL;
+
+    while ((data = elf_getdata(scn, data)) != NULL) {
+	offset = 0;
+
+	for (int i = shdr->sh_info; --i >= 0; ) {
+	    GElf_Verdef def_mem, *def;
+	    def = gelf_getverdef (data, offset, &def_mem);
+	    if (def == NULL)
+		break;
+	    auxoffset = offset + def->vd_aux;
+
+	    for (int j = def->vd_cnt; --j >= 0; ) {
+		GElf_Verdaux aux_mem, * aux;
+		const char *s;
+		aux = gelf_getverdaux (data, auxoffset, &aux_mem);
+		if (aux == NULL)
+		    break;
+		s = elf_strptr(ei->elf, shdr->sh_link, aux->vda_name);
+		if (s == NULL)
+		    break;
+		if (def->vd_flags & VER_FLG_BASE) {
+		    rfree(soname);
+		    soname = rstrdup(s);
+		    auxoffset += aux->vda_next;
+		    continue;
+		} else if (soname && !skipPrivate(s)) {
+		    const char *marker = ei->isElf64 ? "(64bit)" : "";
+		    char *dep = NULL;
+		    rasprintf(&dep, "%s(%s)%s", soname, s, marker);
+		    argvAdd(&ei->provides, dep);
+		    rfree(dep);
+		}
+	    }
+		    
+	}
+    }
+    rfree(soname);
+}
+
+static void processVerNeed(Elf_Scn *scn, GElf_Shdr *shdr, elfInfo *ei)
+{
+    Elf_Data *data;
+    char *soname = NULL;
+    while ((data = elf_getdata(scn, data)) != NULL) {
+	unsigned int offset = 0, auxoffset;
+	for (int i = shdr->sh_info; --i >= 0; ) {
+	    const char *s = NULL;
+	    GElf_Verneed need_mem, *need;
+	    need = gelf_getverneed (data, offset, &need_mem);
+	    if (need == NULL)
+		break;
+
+	    s = elf_strptr(ei->elf, shdr->sh_link, need->vn_file);
+	    if (s == NULL)
+		break;
+	    rfree(soname);
+	    soname = rstrdup(s);
+	    auxoffset = offset + need->vn_aux;
+
+	    for (int j = need->vn_cnt; --j >= 0; ) {
+		GElf_Vernaux aux_mem, * aux;
+		aux = gelf_getvernaux (data, auxoffset, &aux_mem);
+		if (aux == NULL)
+		    break;
+		s = elf_strptr(ei->elf, shdr->sh_link, aux->vna_name);
+		if (s == NULL)
+		    break;
+
+		if (ei->isExec && soname && !skipPrivate(s)) {
+		    const char *marker = ei->isElf64 ? "(64bit)" : "";
+		    char *dep = NULL;
+		    rasprintf(&dep, "%s(%s)%s", soname, s, marker);
+		    argvAdd(&ei->requires, dep);
+		    rfree(dep);
+		}
+		auxoffset += aux->vna_next;
+	    }
+	    offset += need->vn_next;
+	}
+    }
+    rfree(soname);
+}
+
+static void processDynamic(Elf_Scn *scn, GElf_Shdr *shdr, elfInfo *ei)
+{
+    Elf_Data *data;
+    while ((data = elf_getdata(scn, data)) != NULL) {
+	for (int i = 0; i < (shdr->sh_size / shdr->sh_entsize); i++) {
+	    ARGV_t *deptype = NULL;
+	    const char *s = NULL;
+	    GElf_Dyn dyn_mem, *dyn;
+
+	    dyn = gelf_getdyn (data, i, &dyn_mem);
+	    if (dyn == NULL)
+		break;
+
+	    switch (dyn->d_tag) {
+	    case DT_HASH:
+		ei->gotHASH = 1;
+		break;
+	    case DT_GNU_HASH:
+		ei->gotGNUHASH = 1;
+		break;
+	    case DT_DEBUG:
+		ei->gotDEBUG = 1;
+		break;
+	    case DT_SONAME:
+		ei->gotSONAME = 1;
+		s = elf_strptr(ei->elf, shdr->sh_link, dyn->d_un.d_val);
+		deptype = &ei->provides;
+		break;
+	    case DT_NEEDED:
+		if (ei->isExec) {
+		    s = elf_strptr(ei->elf, shdr->sh_link, dyn->d_un.d_val);
+		    deptype = &ei->requires;
+		}
+		break;
+	    default:
+		break;
+	    }
+
+	    if (s && deptype) {
+		const char *marker = ei->isElf64 ? "()(64bit)" : "";
+		char *dep = rstrscat(NULL, s, marker, NULL);
+		argvAdd(deptype, dep);
+		rfree(dep);
+	    }
+	}
+    }
+}
+
+static void processSections(elfInfo *ei)
+{
+    Elf_Scn * scn;
+    while ((scn = elf_nextscn(ei->elf, scn)) != NULL) {
+	GElf_Shdr shdr_mem, *shdr;
+	shdr = gelf_getshdr(scn, &shdr_mem);
+	if (shdr == NULL)
+	    break;
+
+	switch (shdr->sh_type) {
+	case SHT_GNU_verdef:
+	    processVerDef(scn, shdr, ei);
+	    break;
+	case SHT_GNU_verneed:
+	    processVerNeed(scn, shdr, ei);
+	    break;
+	case SHT_DYNAMIC:
+	    processDynamic(scn, shdr, ei);
+	    break;
+	default:
+	    break;
+	}
+    }
+}
+
+static int processFile(const char *fn, int dtype)
+{
+    int rc = 1;
+    int fdno = -1;
+    struct stat st;
+    GElf_Ehdr *ehdr, ehdr_mem;
+    elfInfo *ei = rcalloc(1, sizeof(*ei));
+
+    fdno = open(fn, O_RDONLY);
+    if (fdno < 0 || fstat(fdno, &st) < 0)
+	goto exit;
+
+    (void) elf_version(EV_CURRENT);
+    ei->elf = elf_begin(fdno, ELF_C_READ, NULL);
+    if (ei->elf == NULL || elf_kind(ei->elf) != ELF_K_ELF)
+	goto exit;
+
+    ehdr = gelf_getehdr(ei->elf, &ehdr_mem);
+    if (ehdr == NULL)
+	goto exit;
+
+    if (ehdr->e_type == ET_DYN || ehdr->e_type == ET_EXEC) {
+/* on alpha, everything is 64bit but we dont want the (64bit) markers */
+#if !defined(__alpha__)
+    	ei->isElf64 = (ehdr->e_ident[EI_CLASS] == ELFCLASS64);
+#else
+	ei->isElf64 = 0;
+#endif
+    	ei->isDSO = (ehdr->e_type == ET_DYN);
+	ei->isExec = (st.st_mode & (S_IXUSR|S_IXGRP|S_IXOTH));
+
+	processSections(ei);
+    }
+
+    /*
+     * For DSOs which use the .gnu_hash section and don't have a .hash
+     * section, we need to ensure that we have a new enough glibc.
+     */
+    if (ei->isExec && ei->gotGNUHASH && !ei->gotHASH) {
+	argvAdd(&ei->requires, "rtld(GNU_HASH)");
+    }
+
+    /* For DSO's, provide the basename of the file if DT_SONAME not found. */
+    if (ei->isDSO && !ei->gotDEBUG && !ei->gotSONAME) {
+	const char *marker = ei->isElf64 ? "()(64bit)" : "";
+	const char *bn = strrchr(fn, '/');
+	char *dep;
+	bn = bn ? bn + 1 : fn;
+	dep = rstrscat(NULL, bn, marker, NULL);
+	argvAdd(&ei->requires, dep);
+	rfree(dep);
+    }
+
+    rc = 0;
+    /* dump the requested dependencies for this file */
+    for (ARGV_t dep = dtype ? ei->requires : ei->provides; dep && *dep; dep++) {
+	fprintf(stdout, "%s\n", *dep);
+    }
+
+exit:
+    if (fdno >= 0) close(fdno);
+    if (ei) {
+	argvFree(ei->provides);
+	argvFree(ei->requires);
+    	if (ei->elf) elf_end(ei->elf);
+	rfree(ei);
+    }
+    return rc;
+}
+
+int main(int argc, char *argv[])
+{
+    char fn[BUFSIZ];
+    int provides = 0;
+    int requires = 0;
+    poptContext optCon;
+
+    struct poptOption opts[] = {
+	{ "provides", 'P', POPT_ARG_VAL, &provides, -1, NULL, NULL },
+	{ "requires", 'R', POPT_ARG_VAL, &requires, -1, NULL, NULL },
+	{ "filter-private", 0, POPT_ARG_VAL, &filter_private, -1, NULL, NULL },
+	POPT_AUTOHELP 
+	POPT_TABLEEND
+    };
+
+    optCon = poptGetContext(argv[0], argc, (const char **) argv, opts, 0);
+    if (argc < 2 || poptGetNextOpt(optCon) == 0) {
+	poptPrintUsage(optCon, stderr, 0);
+	exit(EXIT_FAILURE);
+    }
+
+    while (fgets(fn, sizeof(fn), stdin) != NULL) {
+	fn[strlen(fn)-1] = '\0';
+	(void) processFile(fn, requires);
+    }
+
+    poptFreeContext(optCon);
+    return 0;
+}

Makefile.am
@@ -32,7 +32,6 @@ AM_CPPFLAGS += -I$(top_srcdir)/rpmio
 AM_CPPFLAGS += @WITH_NSS_INCLUDE@
 AM_CPPFLAGS += @WITH_POPT_INCLUDE@
 AM_CPPFLAGS += -I$(top_srcdir)/misc
-AM_CPPFLAGS += @WITH_LIBELF_INCLUDE@
 AM_CPPFLAGS += -DLOCALEDIR="\"$(localedir)\""
 AM_CPPFLAGS += -DLIBRPMALIAS_FILENAME="\"rpmpopt-${VERSION}\""
 
@@ -88,16 +87,16 @@ DISTCLEANFILES += find-requires
 rpm_SOURCES =		rpmqv.c debug.h system.h
 rpm_CPPFLAGS =		$(AM_CPPFLAGS) -DIAM_RPMDB -DIAM_RPMEIU -DIAM_RPMK -DIAM_RPMQV
 rpm_LDADD =		build/librpmbuild.la lib/librpm.la rpmio/librpmio.la
-rpm_LDADD +=		@WITH_LIBELF_LIB@ @WITH_NSS_LIB@ @WITH_POPT_LIB@ @WITH_ZLIB_LIB@
+rpm_LDADD +=		@WITH_NSS_LIB@ @WITH_POPT_LIB@ @WITH_ZLIB_LIB@
 
 rpmbuild_SOURCES =	build.c rpmqv.c build.h debug.h system.h
 rpmbuild_CPPFLAGS =	$(AM_CPPFLAGS) -DIAM_RPMBT
 rpmbuild_LDADD =	build/librpmbuild.la lib/librpm.la rpmio/librpmio.la
-rpmbuild_LDADD += 	@WITH_LIBELF_LIB@ @WITH_NSS_LIB@ @WITH_POPT_LIB@ @WITH_ZLIB_LIB@
+rpmbuild_LDADD += 	@WITH_NSS_LIB@ @WITH_POPT_LIB@ @WITH_ZLIB_LIB@
 
 rpm2cpio_SOURCES =	rpm2cpio.c debug.h system.h
 rpm2cpio_LDADD =	lib/librpm.la rpmio/librpmio.la
-rpm2cpio_LDADD +=	@WITH_LIBELF_LIB@ @WITH_NSS_LIB@ @WITH_POPT_LIB@ @WITH_ZLIB_LIB@
+rpm2cpio_LDADD +=	@WITH_NSS_LIB@ @WITH_POPT_LIB@ @WITH_ZLIB_LIB@
 
 
 if LIBELF
@@ -108,6 +107,11 @@ rpmlibexec_PROGRAMS +=	debugedit
 debugedit_SOURCES =	tools/debugedit.c tools/hashtab.c tools/hashtab.h
 debugedit_LDADD =	rpmio/librpmio.la
 debugedit_LDADD +=	@WITH_LIBELF_LIB@ @WITH_POPT_LIB@
+
+rpmlibexec_PROGRAMS +=	elfdeps
+elfdeps_SOURCES =	tools/elfdeps.c
+elfdeps_LDADD =		rpmio/librpmio.la
+elfdeps_LDADD +=	@WITH_LIBELF_LIB@ @WITH_POPT_LIB@
 endif
 endif
 

build/Makefile.am
@@ -4,7 +4,6 @@ AM_CPPFLAGS =  -I$(top_builddir) -I$(top_srcdir) -I$(top_builddir)/include/
 AM_CPPFLAGS += @WITH_NSS_INCLUDE@
 AM_CPPFLAGS += @WITH_MAGIC_INCLUDE@
 AM_CPPFLAGS += @WITH_POPT_INCLUDE@
-AM_CPPFLAGS += @WITH_LIBELF_INCLUDE@
 AM_CPPFLAGS += -I$(top_srcdir)/misc
 
 usrlibdir = $(libdir)
@@ -19,5 +18,4 @@ librpmbuild_la_LIBADD = \
 	$(top_builddir)/lib/librpm.la \
 	$(top_builddir)/rpmio/librpmio.la \
 	@WITH_POPT_LIB@ \
-	@WITH_LIBELF_LIB@ \
 	@WITH_MAGIC_LIB@

build/rpmfc.c
@@ -6,15 +6,6 @@
 #include <signal.h>
 #include <magic.h>
 
-#if HAVE_GELF_H
-#include <gelf.h>
-
-#if !defined(DT_GNU_HASH)
-#define	DT_GNU_HASH		0x6ffffef5
-#endif
-
-#endif
-
 #include <rpm/header.h>
 #include <rpm/rpmbuild.h>
 #include <rpm/argv.h>
@@ -746,302 +737,7 @@ static int rpmfcSCRIPT(rpmfc fc)
  */
 static int rpmfcELF(rpmfc fc)
 {
-#if HAVE_GELF_H && HAVE_LIBELF
-    const char * fn = fc->fn[fc->ix];
-    Elf * elf;
-    Elf_Scn * scn;
-    Elf_Data * data;
-    GElf_Ehdr ehdr_mem, * ehdr;
-    GElf_Shdr shdr_mem, * shdr;
-    GElf_Verdef def_mem, * def;
-    GElf_Verneed need_mem, * need;
-    GElf_Dyn dyn_mem, * dyn;
-    unsigned int auxoffset;
-    unsigned int offset;
-    int fdno;
-    int cnt2;
-    int cnt;
-    char *buf = NULL;
-    const char * s;
-    struct stat sb, * st = &sb;
-    char * soname = NULL;
-    rpmds * depsp, ds;
-    rpmTag tagN;
-    rpmsenseFlags dsContext;
-    int xx;
-    int isElf64;
-    int isDSO;
-    int gotSONAME = 0;
-    int gotDEBUG = 0;
-    int gotHASH = 0;
-    int gotGNUHASH = 0;
-    static int filter_GLIBC_PRIVATE = 0;
-    static int oneshot = 0;
-
-    if (oneshot == 0) {
-	oneshot = 1;
-	filter_GLIBC_PRIVATE = rpmExpandNumeric("%{?_filter_GLIBC_PRIVATE}");
-    }
-
-    /* Files with executable bit set only. */
-    if (stat(fn, st) != 0)
-	return(-1);
-
-    fdno = open(fn, O_RDONLY);
-    if (fdno < 0)
-	return fdno;
-
-    (void) elf_version(EV_CURRENT);
-
-    elf = NULL;
-    if ((elf = elf_begin (fdno, ELF_C_READ, NULL)) == NULL
-     || elf_kind(elf) != ELF_K_ELF
-     || (ehdr = gelf_getehdr(elf, &ehdr_mem)) == NULL
-     || !(ehdr->e_type == ET_DYN || ehdr->e_type == ET_EXEC))
-	goto exit;
-
-    isElf64 = ehdr->e_ident[EI_CLASS] == ELFCLASS64;
-    isDSO = ehdr->e_type == ET_DYN;
-
-    scn = NULL;
-    while ((scn = elf_nextscn(elf, scn)) != NULL) {
-	shdr = gelf_getshdr(scn, &shdr_mem);
-	if (shdr == NULL)
-	    break;
-
-	soname = _free(soname);
-	switch (shdr->sh_type) {
-	default:
-	    continue;
-	    break;
-	case SHT_GNU_verdef:
-	    data = NULL;
-	    if (!fc->skipProv)
-	    while ((data = elf_getdata (scn, data)) != NULL) {
-		offset = 0;
-		for (cnt = shdr->sh_info; --cnt >= 0; ) {
-		
-		    def = gelf_getverdef (data, offset, &def_mem);
-		    if (def == NULL)
-			break;
-		    auxoffset = offset + def->vd_aux;
-		    for (cnt2 = def->vd_cnt; --cnt2 >= 0; ) {
-			GElf_Verdaux aux_mem, * aux;
-
-			aux = gelf_getverdaux (data, auxoffset, &aux_mem);
-			if (aux == NULL)
-			    break;
-
-			s = elf_strptr(elf, shdr->sh_link, aux->vda_name);
-			if (s == NULL)
-			    break;
-			if (def->vd_flags & VER_FLG_BASE) {
-			    soname = _free(soname);
-			    soname = xstrdup(s);
-			    auxoffset += aux->vda_next;
-			    continue;
-			} else
-			if (soname != NULL
-			 && !(filter_GLIBC_PRIVATE != 0
-				&& rstreq(s, "GLIBC_PRIVATE")))
-			{
-			    rasprintf(&buf, "%s(%s)%s", soname, s,
-#if !defined(__alpha__)
-							isElf64 ? "(64bit)" : "");
-#else
-							"");
-#endif
-
-			    /* Add to package provides. */
-			    ds = rpmdsSingle(RPMTAG_PROVIDES,
-					buf, "", RPMSENSE_FIND_PROVIDES);
-			    xx = rpmdsMerge(&fc->provides, ds);
-
-			    /* Add to file dependencies. */
-			    rpmfcAddFileDep(&fc->ddict, fc->ix, ds);
-
-			    ds = rpmdsFree(ds);
-			    free(buf);
-			}
-			auxoffset += aux->vda_next;
-		    }
-		    offset += def->vd_next;
-		}
-	    }
-	    break;
-	case SHT_GNU_verneed:
-	    data = NULL;
-	    /* Files with executable bit set only. */
-	    if (!fc->skipReq && (st->st_mode & (S_IXUSR|S_IXGRP|S_IXOTH)))
-	    while ((data = elf_getdata (scn, data)) != NULL) {
-		offset = 0;
-		for (cnt = shdr->sh_info; --cnt >= 0; ) {
-		    need = gelf_getverneed (data, offset, &need_mem);
-		    if (need == NULL)
-			break;
-
-		    s = elf_strptr(elf, shdr->sh_link, need->vn_file);
-		    if (s == NULL)
-			break;
-		    soname = _free(soname);
-		    soname = xstrdup(s);
-		    auxoffset = offset + need->vn_aux;
-		    for (cnt2 = need->vn_cnt; --cnt2 >= 0; ) {
-			GElf_Vernaux aux_mem, * aux;
-
-			aux = gelf_getvernaux (data, auxoffset, &aux_mem);
-			if (aux == NULL)
-			    break;
-
-			s = elf_strptr(elf, shdr->sh_link, aux->vna_name);
-			if (s == NULL)
-			    break;
-
-			/* Filter dependencies that contain GLIBC_PRIVATE */
-			if (soname != NULL
-			 && !(filter_GLIBC_PRIVATE != 0
-				&& rstreq(s, "GLIBC_PRIVATE")))
-			{
-			    rasprintf(&buf, "%s(%s)%s", soname, s,
-#if !defined(__alpha__)
-							isElf64 ? "(64bit)" : "");
-#else
-							"");
-#endif
-
-			    /* Add to package dependencies. */
-			    ds = rpmdsSingle(RPMTAG_REQUIRENAME,
-					buf, "", RPMSENSE_FIND_REQUIRES);
-			    xx = rpmdsMerge(&fc->requires, ds);
-
-			    /* Add to file dependencies. */
-			    rpmfcAddFileDep(&fc->ddict, fc->ix, ds);
-			    ds = rpmdsFree(ds);
-			    free(buf);
-			}
-			auxoffset += aux->vna_next;
-		    }
-		    offset += need->vn_next;
-		}
-	    }
-	    break;
-	case SHT_DYNAMIC:
-	    data = NULL;
-	    while ((data = elf_getdata (scn, data)) != NULL) {
-		for (cnt = 0; cnt < (shdr->sh_size / shdr->sh_entsize); ++cnt) {
-		    dyn = gelf_getdyn (data, cnt, &dyn_mem);
-		    if (dyn == NULL)
-			break;
-		    s = NULL;
-		    switch (dyn->d_tag) {
-		    default:
-			continue;
-			break;
-                    case DT_HASH:    
-			gotHASH= 1;
-			continue;
-                    case DT_GNU_HASH:
-			gotGNUHASH= 1;
-			continue;
-                    case DT_DEBUG:    
-			gotDEBUG = 1;
-			continue;
-		    case DT_NEEDED:
-			/* Files with executable bit set only. */
-			if (fc->skipReq || !(st->st_mode & (S_IXUSR|S_IXGRP|S_IXOTH)))
-			    continue;
-			/* Add to package requires. */
-			depsp = &fc->requires;
-			tagN = RPMTAG_REQUIRENAME;
-			dsContext = RPMSENSE_FIND_REQUIRES;
-			s = elf_strptr(elf, shdr->sh_link, dyn->d_un.d_val);
-assert(s != NULL);
-			break;
-		    case DT_SONAME:
-			gotSONAME = 1;
-			/* Add to package provides. */
-			if (fc->skipProv)
-			    continue;
-			depsp = &fc->provides;
-			tagN = RPMTAG_PROVIDENAME;
-			dsContext = RPMSENSE_FIND_PROVIDES;
-			s = elf_strptr(elf, shdr->sh_link, dyn->d_un.d_val);
-assert(s != NULL);
-			break;
-		    }
-		    if (s == NULL)
-			continue;
-
-		    rasprintf(&buf, "%s%s", s,
-#if !defined(__alpha__)
-					    isElf64 ? "()(64bit)" : "");
-#else
-					    "");
-#endif
-
-		    /* Add to package dependencies. */
-		    ds = rpmdsSingle(tagN, buf, "", dsContext);
-		    xx = rpmdsMerge(depsp, ds);
-
-		    /* Add to file dependencies. */
-		    rpmfcAddFileDep(&fc->ddict, fc->ix, ds);
-
-		    ds = rpmdsFree(ds);
-		    free(buf);
-		}
-	    }
-	    break;
-	}
-    }
-
-    /* For DSOs which use the .gnu_hash section and don't have a .hash
-     * section, we need to ensure that we have a new enough glibc. */ 
-    if (gotGNUHASH && !gotHASH) {
-        ds = rpmdsSingle(RPMTAG_REQUIRENAME, "rtld(GNU_HASH)", "", 
-                         RPMSENSE_FIND_REQUIRES);
-        rpmdsMerge(&fc->requires, ds);
-        rpmfcAddFileDep(&fc->ddict, fc->ix, ds);
-        ds = rpmdsFree(ds);
-    }
-
-    /* For DSO's, provide the basename of the file if DT_SONAME not found. */
-    if (!fc->skipProv && isDSO && !gotDEBUG && !gotSONAME) {
-	depsp = &fc->provides;
-	tagN = RPMTAG_PROVIDENAME;
-	dsContext = RPMSENSE_FIND_PROVIDES;
-
-	s = strrchr(fn, '/');
-	if (s)
-	    s++;
-	else
-	    s = fn;
-
-	rasprintf(&buf, "%s%s", s,
-#if !defined(__alpha__)
-				isElf64 ? "()(64bit)" : "");
-#else
-				"");
-#endif
-
-	/* Add to package dependencies. */
-	ds = rpmdsSingle(tagN, buf, "", dsContext);
-	xx = rpmdsMerge(depsp, ds);
-
-	/* Add to file dependencies. */
-	rpmfcAddFileDep(&fc->ddict, fc->ix, ds);
-
-	ds = rpmdsFree(ds);
-	free(buf);
-    }
-
-exit:
-    soname = _free(soname);
-    if (elf) (void) elf_end(elf);
-    xx = close(fdno);
-    return 0;
-#else
-    return -1;
-#endif
+    return rpmfcGenDeps(fc, "elf", (DEP_REQ|DEP_PROV));
 }
 
 static int rpmfcMISC(rpmfc fc)

configure.ac
@@ -225,7 +225,6 @@ AC_CHECK_LIB(port, writev)
 
 #=================
 # Check for libelf library. Prefer external, otherwise none.
-WITH_LIBELF_INCLUDE=
 WITH_LIBELF_LIB=
 AC_CHECK_HEADER([libelf.h])
 AC_CHECK_HEADERS([gelf.h], [
@@ -235,7 +234,6 @@ AC_CHECK_HEADERS([gelf.h], [
 	    WITH_LIBELF=yes
 	])
 ])
-AC_SUBST(WITH_LIBELF_INCLUDE)
 AC_SUBST(WITH_LIBELF_LIB)
 AM_CONDITIONAL(LIBELF,[test "$WITH_LIBELF" = yes])
 

macros.in
@@ -476,7 +476,7 @@ print (t)\
 #%__find_obsoletes	???
 
 #
-# Path to scripts to autogenerate per-interpreter package dependencies.
+# Path to scripts to autogenerate per-filetype package dependencies.
 # If the script supports extra options they can be passed by defining 
 # <helper_macro>_opts macro(s), eg in spec:
 # %define __ocaml_requires_opts -i Warnings
@@ -487,6 +487,9 @@ print (t)\
 #%__perl_requires	%{_rpmconfigdir}/perldeps.pl --requires
 
 %__interpreter_requires	%{_rpmconfigdir}/interpreter.req
+
+%__elf_provides		%{_rpmconfigdir}/elfdeps --provides %{?__filter_GLIBC_PRIVATE:--filter-private}
+%__elf_requires		%{_rpmconfigdir}/elfdeps --requires %{?__filter_GLIBC_PRIVATE:--filter-private}
 
 %__perl_provides	%{_rpmconfigdir}/perl.prov
 %__perl_requires	%{_rpmconfigdir}/perl.req

rpmio/Makefile.am
@@ -5,7 +5,6 @@ AM_CPPFLAGS += @WITH_NSS_INCLUDE@
 AM_CPPFLAGS += @WITH_LUA_INCLUDE@
 AM_CPPFLAGS += @WITH_POPT_INCLUDE@
 AM_CPPFLAGS += -I$(top_srcdir)/misc
-AM_CPPFLAGS += @WITH_LIBELF_INCLUDE@
 AM_CPPFLAGS += -DRPMCONFIGDIR="\"@RPMCONFIGDIR@\""
 AM_CPPFLAGS += -DLOCALSTATEDIR="\"$(localstatedir)\""
 

Conan-Kudo/rpm-histfix

commit 0f02dc2228e348a190939dcaaf68015ea558d470

Gah, stupid thinko in elfdeps - for DSO's without a soname, we should be adding a *provide* of the   basename, not require - dumb regression from commit 87c237bebfc792a8b439fa2c5a2b78328b139d50

tools/elfdeps.c
@@ -244,7 +244,7 @@ static int processFile(const char *fn, int dtype)
 	char *dep;
 	bn = bn ? bn + 1 : fn;
 	dep = rstrscat(NULL, bn, marker, NULL);
-	argvAdd(&ei->requires, dep);
+	argvAdd(&ei->provides, dep);
 	rfree(dep);
     }
 