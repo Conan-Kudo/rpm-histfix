rpm-software-management/rpm

commit 7309f7498134f7b62f27624a4cdb91e1c6b82578

Lose pointless per-db and per-dbi errfile and errcall pointers - BDB uses stderr for errfile when not set, which is what we set it   to through various hoops. Just leave it alone. - db_errcall has always been (intentionally) NULL to disable that output,   the errors get logged though cvtdberr() with slight filtering

lib/backend/db3.c
@@ -101,10 +101,6 @@ static int db_init(dbiIndex dbi, const char * dbhome, DB_ENV ** dbenvp)
     if (dbenvp == NULL)
 	return 1;
 
-    /* XXX HACK */
-    if (rpmdb->db_errfile == NULL)
-	rpmdb->db_errfile = stderr;
-
     eflags = (dbi->dbi_oeflags | dbi->dbi_eflags);
     if (eflags & DB_JOINENV) eflags &= DB_JOINENV;
 
@@ -128,8 +124,7 @@ static int db_init(dbiIndex dbi, const char * dbhome, DB_ENV ** dbenvp)
 	goto errxit;
 
     dbenv->set_alloc(dbenv, rmalloc, rrealloc, NULL);
-    dbenv->set_errcall(dbenv, (void *) rpmdb->db_errcall);
-    dbenv->set_errfile(dbenv, rpmdb->db_errfile);
+    dbenv->set_errcall(dbenv, NULL);
     dbenv->set_errpfx(dbenv, rpmdb->db_errpfx);
 
 #if (DB_VERSION_MAJOR >= 4 && DB_VERSION_MINOR >= 5)
@@ -441,8 +436,7 @@ int dbiClose(dbiIndex dbi, unsigned int flags)
 	rc = cvtdberr(dbi, "db_env_create", rc, _debug);
 	if (rc || dbenv == NULL) goto exit;
 
-	dbenv->set_errcall(dbenv, (void *) rpmdb->db_errcall);
-	dbenv->set_errfile(dbenv, rpmdb->db_errfile);
+	dbenv->set_errcall(dbenv, NULL);
 	dbenv->set_errpfx(dbenv, rpmdb->db_errpfx);
 #if !(DB_VERSION_MAJOR == 4 && DB_VERSION_MINOR >= 3)
 	xx = dbenv->set_verbose(dbenv, DB_VERB_CHKPOINT,

lib/rpmdb_internal.h
@@ -72,8 +72,6 @@ struct _dbiIndex {
 	/* dbenv parameters */
     int	dbi_lorder;
     /* XXX db-4.3.14 adds dbenv as 1st arg. */
-    void (*db_errcall) (void * dbenv, const char *db_errpfx, char *buffer);
-    FILE *	dbi_errfile;
     int	dbi_verbose;
 	/* mpool sub-system parameters */
     int	dbi_mmapsize;	/*!< (10Mb) */
@@ -126,8 +124,6 @@ struct rpmdb_s {
     int		db_remove_env;
     int		db_chrootDone;	/*!< If chroot(2) done, ignore db_root. */
     int		db_mkdirDone;	/*!< Has db_home been created? */
-    void (*db_errcall) (const char *db_errpfx, char *buffer);
-    FILE *	db_errfile;
     unsigned char * db_bits;	/*!< package instance bit mask. */
     int		db_nbits;	/*!< no. of bits in mask. */
     rpmdb	db_next;

Conan-Kudo/rpm-histfix

commit 5fbc01fee38546aac14335940ef5de8c4da7cc9e

Add + use a db error callback function - Commit 7309f7498134f7b62f27624a4cdb91e1c6b82578 caused loss of   some BDB error messages due misreading the docs: we used to explicitly   set errfile to stderr through some hoops, and errcall to what   always was NULL. But calling set_errcall() disables any output to   the db errfile, and since we're no longer calling errfile... - Removing the set_errcall() would restore output on stderr, but   making it go through rpmlog() seems more consistent and lets   API users catch it through rpmlog callback (in theory at least) (cherry picked from commit ece9fbf5be8d93d28dbb114832a531dde03cfed2)

lib/backend/db3.c
@@ -39,6 +39,11 @@ static int cvtdberr(dbiIndex dbi, const char * msg, int error, int printit)
     return dbapi_err(dbi->dbi_rpmdb, msg, error, printit);
 }
 
+static void errlog(DB_ENV * env, const char *errpfx, const char *msg)
+{
+    rpmlog(RPMLOG_ERR, "%s: %s\n", errpfx, msg);
+}
+
 static uint32_t db_envflags(DB * db)
 {
     DB_ENV * env = db->get_env(db);
@@ -136,7 +141,7 @@ static int db_init(rpmdb rdb, const char * dbhome)
 	goto errxit;
 
     dbenv->set_alloc(dbenv, rmalloc, rrealloc, NULL);
-    dbenv->set_errcall(dbenv, NULL);
+    dbenv->set_errcall(dbenv, errlog);
     dbenv->set_errpfx(dbenv, _errpfx);
 
     /* 