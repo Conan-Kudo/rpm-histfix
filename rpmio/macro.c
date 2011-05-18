rpm-software-management/rpm

commit ebc4ceaaeb8bb59019f4635471b28eb5f3eaaaa6

Avoid unnecessary round-trip through expandT() where possible - In the cases where expandT() was called with strlen(source) we can   now just bypass it and call expandMacro() directly, avoiding an   unnecessary string copy.

rpmio/macro.c
@@ -1008,7 +1008,7 @@ doFoo(MacroBuf mb, int negate, const char * f, size_t fn,
     }
 
     if (b) {
-	(void) expandT(mb, b, strlen(b));
+	(void) expandMacro(mb, b);
     }
     free(buf);
 }
@@ -1285,7 +1285,7 @@ expandMacro(MacroBuf mb, const char *src)
 			rc = expandT(mb, g, gn);
 		} else
 		if (me && me->body && *me->body) {/* Expand %{-f}/%{-f*} */
-			rc = expandT(mb, me->body, strlen(me->body));
+			rc = expandMacro(mb, me->body);
 		}
 		s = se;
 		continue;
@@ -1302,7 +1302,7 @@ expandMacro(MacroBuf mb, const char *src)
 			rc = expandT(mb, g, gn);
 		} else
 		if (me && me->body && *me->body) { /* Expand %{?f}/%{?f*} */
-			rc = expandT(mb, me->body, strlen(me->body));
+			rc = expandMacro(mb, me->body);
 		}
 		s = se;
 		continue;

Conan-Kudo/rpm-histfix

commit 2160bd2e46d2c595ea2a20aa4544d83578bb4c2f

Always copy macro source when expanding it - A macro can undefine itself, and unless we grab a copy of it we'll   end up accessing already freed memory. Fixes a regression from   commit ebc4ceaaeb8bb59019f4635471b28eb5f3eaaaa6 which assumed   a copy is not always needed.
Signed-off-by: Panu Matilainen <pmatilai@redhat.com>

rpmio/macro.c
@@ -1022,12 +1022,12 @@ expandMacro(MacroBuf mb, const char *src, size_t slen)
     char *source = NULL;
 
     /* Handle non-terminated substrings by creating a terminated copy */
-    if (slen > 0) {
-	source = xmalloc(slen + 1);
-	strncpy(source, src, slen);
-	source[slen] = '\0';
-	s = source;
-    }
+    if (!slen)
+	slen = strlen(src);
+    source = xmalloc(slen + 1);
+    strncpy(source, src, slen);
+    source[slen] = '\0';
+    s = source;
 
     if (mb->buf == NULL) {
 	size_t blen = MACROBUFSIZ + strlen(s);