rpm-software-management/rpm

commit 7e4415fcc5e11cfd4cd9d0dfe19568be73f15d74

Error/notfound case is handled the same for both these cases

lib/rpmdb.c
@@ -1973,10 +1973,6 @@ rpmdbMatchIterator rpmdbInitIterator(rpmdb db, rpmDbiTagVal rpmtag,
             } else {
 		rc = dbiGetToSet(dbi, keyp, keylen, &set);
 	    }
-            if (rc)	{	/* error/not found */
-                set = dbiIndexSetFree(set);
-                goto exit;
-            }
 	} else {
             /* get all entries from index */
 	    dbiCursor dbc = dbiCursorInit(dbi, 0);
@@ -2000,12 +1996,12 @@ rpmdbMatchIterator rpmdbInitIterator(rpmdb db, rpmDbiTagVal rpmtag,
             }
 
 	    dbiCursorFree(dbc);
-
-            if (rc != DB_NOTFOUND)	{	/* error */
-                set = dbiIndexSetFree(set);
-                goto exit;
-            }
         }
+
+	if (rc)	{	/* error/not found */
+	    set = dbiIndexSetFree(set);
+	    goto exit;
+	}
     }
 
     /* Copy the retrieval key, byte swapping header instance if necessary. */

Conan-Kudo/rpm-histfix

commit 143cc7e092d98e4f73d56d7ab161bc1860aa195b

Add a bunch of db match/key iterator test-cases in python - This would've caught the breakage introduced in commit   7e4415fcc5e11cfd4cd9d0dfe19568be73f15d74, but better late than never...

tests/rpmpython.at
@@ -40,3 +40,75 @@ for e in ts:
 ],
 [foo-1.0-1.noarch]
 )
+
+AT_SETUP([database iterators])
+AT_KEYWORDS([python rpmdb])
+AT_CHECK([
+RPMDB_CLEAR
+RPMDB_INIT
+runroot rpm -i \
+  --justdb --nodeps --ignorearch --ignoreos \
+  /data/RPMS/foo-1.0-1.noarch.rpm \
+  /data/RPMS/hello-2.0-1.i686.rpm
+],
+[0],
+[],
+[])
+
+RPMPY_CHECK([
+ts = rpm.ts()
+for h in ts.dbMatch('name'):
+    myprint(h['nevra'])
+],
+[foo-1.0-1.noarch
+hello-2.0-1.i686
+],
+[])
+
+RPMPY_CHECK([
+ts = rpm.ts()
+for h in ts.dbMatch('obsoletes'):
+    myprint(h['nevra'])
+],
+[foo-1.0-1.noarch
+],
+[])
+
+RPMPY_CHECK([
+ts = rpm.ts()
+for h in ts.dbMatch('provides', 'hi'):
+    myprint(h['nevra'])
+],
+[foo-1.0-1.noarch
+],
+[])
+
+RPMPY_CHECK([
+ts = rpm.ts()
+for h in ts.dbMatch('basenames', '/usr/share/doc/hello-2.0/FAQ'):
+    myprint(h['nevra'])
+],
+[hello-2.0-1.i686
+],
+[])
+RPMPY_CHECK([
+ts = rpm.ts()
+for di in ts.dbIndex('obsoletes'):
+    myprint(di)
+],
+[howdy
+],
+[])
+
+RPMPY_CHECK([
+ts = rpm.ts()
+for di in ts.dbIndex('provides'):
+    myprint(di)
+],
+[foo
+hello
+hello(x86-32)
+hi
+],
+[])
+AT_CLEANUP