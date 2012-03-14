rpm-software-management/rpm

commit 70f063cb773bedb7d336429d9bc8ed1d4e5d18f4

Make base64 encoding/decoding part of rpmio public API
- Base64 is present in headers and all, it's only reasonable that
  our API users have access to this functionality without having
  to link to other libraries. Even if we didn't want to carry the
  implementation forever in our codebase, we should provide a wrapping
  for this (much like the other crypto stuff) for the reason stated above.
- A bigger issue is that our dirty little (badly hidden) secret was using
  non-namespaced function names, clashing with at least beecrypt. And we
  couldn't have made these internal-only symbols even on platforms that
  support it, because they are used all over the place outside rpmio.
  So... rename the b64 functions to rpmLikeNamingStyle and make 'em public.
  No functional changes, just trivial renaming despite touching numerous
  places.

rpmio/rpmlua.c
@@ -15,8 +15,8 @@
 #include <rpm/rpmlog.h>
 #include <rpm/rpmurl.h>
 #include <rpm/rpmfileutil.h>
+#include <rpm/rpmbase64.h>
 #include "rpmio/rpmhook.h"
-#include "rpmio/base64.h"
 
 #define _RPMLUA_INTERNAL
 #include "rpmio/rpmlua.h"
@@ -588,7 +588,7 @@ void rpmluaInteractive(rpmlua _lua)
 /* ------------------------------------------------------------------ */
 /* Lua API */
 
-static int rpm_b64encode(lua_State *L)
+static int rpm_rpmBase64Encode(lua_State *L)
 {
     const char *str = luaL_checkstring(L, 1);
     size_t len = lua_strlen(L, 1);
@@ -596,20 +596,20 @@ static int rpm_b64encode(lua_State *L)
     if (lua_gettop(L) == 2)
 	linelen = luaL_checkinteger(L, 2);
     if (str && len) {
-	char *data = b64encode(str, len, linelen);
+	char *data = rpmBase64Encode(str, len, linelen);
 	lua_pushstring(L, data);
 	free(data);
     }
     return 1;
 }
 
-static int rpm_b64decode(lua_State *L)
+static int rpm_rpmBase64Decode(lua_State *L)
 {
     const char *str = luaL_checkstring(L, 1);
     if (str) {
 	void *data = NULL;
 	size_t len = 0;
-	if (b64decode(str, &data, &len) == 0) {
+	if (rpmBase64Decode(str, &data, &len) == 0) {
 	    lua_pushlstring(L, data, len);
 	} else {
 	    lua_pushnil(L);
@@ -823,8 +823,8 @@ static int rpm_print (lua_State *L)
 }
 
 static const luaL_reg rpmlib[] = {
-    {"b64encode", rpm_b64encode},
-    {"b64decode", rpm_b64decode},
+    {"rpmBase64Encode", rpm_rpmBase64Encode},
+    {"rpmBase64Decode", rpm_rpmBase64Decode},
     {"expand", rpm_expand},
     {"define", rpm_define},
     {"register", rpm_register},

Makefile.am
@@ -62,6 +62,7 @@ pkginclude_HEADERS += rpmio/rpmsw.h
 pkginclude_HEADERS += rpmio/rpmfileutil.h
 pkginclude_HEADERS += rpmio/rpmutil.h
 pkginclude_HEADERS += rpmio/rpmkeyring.h
+pkginclude_HEADERS += rpmio/rpmbase64.h
 
 pkginclude_HEADERS += lib/header.h
 pkginclude_HEADERS += lib/rpmdb.h

build/files.c
@@ -19,9 +19,9 @@
 #include <rpm/rpmfc.h>
 #include <rpm/rpmfileutil.h>	/* rpmDoDigest() */
 #include <rpm/rpmlog.h>
+#include <rpm/rpmbase64.h>
 
 #include "rpmio/rpmio_internal.h"	/* XXX rpmioSlurp */
-#include "rpmio/base64.h"
 #include "misc/fts.h"
 #include "lib/cpio.h"
 #include "lib/rpmfi_internal.h"	/* XXX fi->apath */

build/policies.c
@@ -9,10 +9,10 @@
 #include <rpm/argv.h>
 #include <rpm/rpmlog.h>
 #include <rpm/rpmpol.h>
+#include <rpm/rpmbase64.h>
 
 #include "rpmio/rpmio_internal.h"
 #include "build/rpmbuild_internal.h"
-#include "rpmio/base64.h"
 
 #include "debug.h"
 
@@ -165,7 +165,7 @@ static ModuleRec newModule(const char *path, const char *name,
 	goto err;
     }
 
-    mod->data = b64encode(raw, rawlen, -1);
+    mod->data = rpmBase64Encode(raw, rawlen, -1);
     if (!mod->data) {
 	rpmlog(RPMLOG_ERR, _("Failed to encode policy file: %s\n"),
 	       mod->path);

lib/formats.c
@@ -305,7 +305,7 @@ static char * armorFormat(rpmtd td)
     case RPM_STRING_TYPE:
     case RPM_STRING_ARRAY_TYPE:
 	enc = rpmtdGetString(td);
-	if (b64decode(enc, (void **)&bs, &ns))
+	if (rpmBase64Decode(enc, (void **)&bs, &ns))
 	    return xstrdup(_("(not base64)"));
 	s = bs;
 	atype = PGPARMOR_PUBKEY;	/* XXX check pkt for pubkey */
@@ -343,7 +343,7 @@ static char * base64Format(rpmtd td)
     if (rpmtdType(td) != RPM_BIN_TYPE) {
 	val = xstrdup(_("(not a blob)"));
     } else {
-	val = b64encode(td->data, td->count, -1);
+	val = rpmBase64Encode(td->data, td->count, -1);
 	if (val == NULL)
 	    val = xstrdup("");
     }

lib/rpmts.c
@@ -319,7 +319,7 @@ static int loadKeyringFromDB(rpmts ts)
 	    uint8_t *pkt;
 	    size_t pktlen;
 
-	    if (b64decode(key, (void **) &pkt, &pktlen) == 0) {
+	    if (rpmBase64Decode(key, (void **) &pkt, &pktlen) == 0) {
 		rpmPubkey key = rpmPubkeyNew(pkt, pktlen);
 		if (rpmKeyringAddKey(ts->keyring, key) == 0) {
 		    char *nvr = headerGetAsString(h, RPMTAG_NVR);

plugins/sepolicy.c
@@ -11,8 +11,8 @@
 #include <rpm/rpmpol.h>
 #include <rpm/rpmfileutil.h>
 #include <rpm/rpmmacro.h>
+#include <rpm/rpmbase64.h>
 
-#include "rpmio/base64.h"
 #include "lib/rpmte_internal.h"
 #include "lib/rpmts_internal.h"	/* rpmtsSELabelFoo() */
 
@@ -216,7 +216,7 @@ static rpmRC sepolWritePolicy(const sepol * pol, char **path)
     size_t policylen;
     rpmRC rc = RPMRC_FAIL;
 
-    if (b64decode(pol->data, (void **) &policy, &policylen) != 0) {
+    if (rpmBase64Decode(pol->data, (void **) &policy, &policylen) != 0) {
 	rpmlog(RPMLOG_ERR, _("Failed to decode policy for %s\n"),
 	       pol->name);
 	goto exit;

preinstall.am
@@ -46,6 +46,10 @@ include/rpm/rpmkeyring.h: rpmio/rpmkeyring.h include/rpm/$(dirstamp)
 	$(INSTALL_DATA) $(top_srcdir)/rpmio/rpmkeyring.h include/rpm/rpmkeyring.h
 BUILT_SOURCES += include/rpm/rpmkeyring.h
 CLEANFILES += include/rpm/rpmkeyring.h
+include/rpm/rpmbase64.h: rpmio/rpmbase64.h include/rpm/$(dirstamp)
+	$(INSTALL_DATA) $(top_srcdir)/rpmio/rpmbase64.h include/rpm/rpmbase64.h
+BUILT_SOURCES += include/rpm/rpmbase64.h
+CLEANFILES += include/rpm/rpmbase64.h
 include/rpm/header.h: lib/header.h include/rpm/$(dirstamp)
 	$(INSTALL_DATA) $(top_srcdir)/lib/header.h include/rpm/header.h
 BUILT_SOURCES += include/rpm/header.h

Conan-Kudo/rpm-histfix

commit 047a9b2dde86aef5a3a222c628966ddec3149ba5

Unbreak lua's base64 encode/decode extensions
- Commit 70f063cb773bedb7d336429d9bc8ed1d4e5d18f4 accidentally
  changed lua's base64 encode/decode interface too, ugh. Dangers of
  search-and-replace... Only the function name string exported to
  lua matters but renaming the internal functions back as well
  for naming consistency.

rpmio/rpmlua.c
@@ -588,7 +588,7 @@ void rpmluaInteractive(rpmlua _lua)
 /* ------------------------------------------------------------------ */
 /* Lua API */
 
-static int rpm_rpmBase64Encode(lua_State *L)
+static int rpm_b64encode(lua_State *L)
 {
     const char *str = luaL_checkstring(L, 1);
     size_t len = lua_strlen(L, 1);
@@ -603,7 +603,7 @@ static int rpm_rpmBase64Encode(lua_State *L)
     return 1;
 }
 
-static int rpm_rpmBase64Decode(lua_State *L)
+static int rpm_b64decode(lua_State *L)
 {
     const char *str = luaL_checkstring(L, 1);
     if (str) {
@@ -823,8 +823,8 @@ static int rpm_print (lua_State *L)
 }
 
 static const luaL_reg rpmlib[] = {
-    {"rpmBase64Encode", rpm_rpmBase64Encode},
-    {"rpmBase64Decode", rpm_rpmBase64Decode},
+    {"b64encode", rpm_b64encode},
+    {"b64decode", rpm_b64decode},
     {"expand", rpm_expand},
     {"define", rpm_define},
     {"register", rpm_register},