rpm-software-management/rpm

commit 06a2f1269b035a3a76464149834f2a5a8c4e89f2

Don't assume rpmfiNew() always succeeds
- Add NULL checks and add/adjust comments where appropriate.
- The remaining callers should handle NULL fi gracefully if not
  entirely correctly: rpmfiFC() returns 0 on NULL fi, so these
  callers just see the erronous file info set as "no files" case.
  Something to fine-tune later...

lib/rpmte.c
@@ -676,6 +676,7 @@ static Header rpmteFDHeader(rpmte te)
 
 static int rpmteOpen(rpmte te, int reload_fi)
 {
+    int rc = 0; /* assume failure */
     Header h = NULL;
     if (te == NULL || te->ts == NULL || rpmteFailed(te))
 	goto exit;
@@ -694,13 +695,16 @@ static int rpmteOpen(rpmte te, int reload_fi)
 	if (reload_fi) {
 	    te->fi = getFI(te, h);
 	}
+
+	/* This can fail if we get a different, bad header from callback */
+	rc = (te->fi != NULL);
 	
 	rpmteSetHeader(te, h);
 	headerFree(h);
     }
 
 exit:
-    return (h != NULL);
+    return rc;
 }
 
 static int rpmteClose(rpmte te, int reset_fi)

build/files.c
@@ -1239,6 +1239,9 @@ static void genCpioListAndHeader(FileList fl,
     headerConvert(h, HEADERCONV_COMPRESSFILELIST);
     fi = rpmfiNew(NULL, h, RPMTAG_BASENAMES, flags);
 
+    if (fi == NULL)
+	return;
+
     /* 
      * Grab the real filenames from ORIGFILENAMES and put into OLDFILENAMES,
      * remove temporary cruft and side-effects from filelist compression 
@@ -1896,6 +1899,8 @@ static rpmRC processPackageFiles(rpmSpec spec, rpmBuildPkgFlags pkgFlags,
 			"PartialHardlinkSets", "4.0.4-1");
 
     genCpioListAndHeader(&fl, &pkg->cpioList, pkg->header, 0);
+    if (pkg->cpioList == NULL)
+	fl.processingFailed = 1;
 
 exit:
     fl.buildRoot = _free(fl.buildRoot);
@@ -2031,9 +2036,13 @@ rpmRC processSourceFiles(rpmSpec spec, rpmBuildPkgFlags pkgFlags)
     argvFree(files);
 
     if (! fl.processingFailed) {
-	if (spec->sourceHeader != NULL)
+	if (spec->sourceHeader != NULL) {
 	    genCpioListAndHeader(&fl, &spec->sourceCpioList,
 			spec->sourceHeader, 1);
+	    if (spec->sourceCpioList == NULL) {
+		fl.processingFailed = 1;
+	    }
+	}
     }
 
     fl.fileList = freeFileList(fl.fileList, fl.fileListRecsUsed);

lib/psm.c
@@ -322,7 +322,7 @@ rpmRC rpmInstallSourcePackage(rpmts ts, FD_t fd,
     fi = rpmfiNew(ts, h, RPMTAG_BASENAMES, RPMFI_KEEPHEADER);
     h = headerFree(h);
 
-    if (fi == NULL) {	/* XXX can't happen */
+    if (fi == NULL) {
 	goto exit;
     }
     fi->apath = filenames.data; /* Ick */

lib/transaction.c
@@ -990,6 +990,7 @@ void checkInstalledFiles(rpmts ts, uint64_t fileCount, rpmFpHash ht, fingerPrint
 		switch (rpmteType(p)) {
 		case TR_ADDED:
 		    if (!otherFi) {
+			/* XXX What to do if this fails? */
 		        otherFi = rpmfiNew(ts, h, RPMTAG_BASENAMES, RPMFI_KEEPHEADER);
 		    }
 		    rpmfiSetFX(fi, recs[j].fileno);

lib/verify.c
@@ -339,6 +339,9 @@ static int verifyHeader(rpmts ts, Header h, rpmVerifyAttrs omitMask, int ghosts)
     int ec = 0;		/* assume no problems */
     rpmfi fi = rpmfiNew(ts, h, RPMTAG_BASENAMES, RPMFI_FLAGS_VERIFY);
 
+    if (fi == NULL)
+	return 1;
+
     rpmfiInit(fi, 0);
     while (rpmfiNext(fi) >= 0) {
 	rpmfileAttrs fileAttrs = rpmfiFFlags(fi);

python/rpmfi-py.c
@@ -307,6 +307,11 @@ static PyObject * rpmfi_new(PyTypeObject * subtype, PyObject *args, PyObject *kw
 
     fi = rpmfiNew(NULL, h, tagN, flags);
 
+    if (fi == NULL) {
+	PyErr_SetString(PyExc_ValueError, "invalid file data in header");
+	return NULL;
+    }
+
     return rpmfi_Wrap(subtype, fi);
 }
 

Conan-Kudo/rpm-histfix

commit 2ec310b7ec87b110aa55345fb8fc1a458897f52d

Transaction element file info can be legally NULL in some cases
- rpmteClose() will wipe out the file info to free memory, we only
  should care whether we failed to (re)load the file info. This
  thinko in commit 06a2f1269b035a3a76464149834f2a5a8c4e89f2
  broke %posttrans scriptlets (and without commit
  274dbf557d1cac90f7c278f9b6d6af05997d92df, %pretrans in other
  circumstances), whoopsie *blush*. Now, off to write a test-case
  for our scriptlet behavior...

lib/rpmte.c
@@ -693,11 +693,12 @@ static int rpmteOpen(rpmte te, int reload_fi)
     }
     if (h != NULL) {
 	if (reload_fi) {
+	    /* This can fail if we get a different, bad header from callback */
 	    te->fi = getFI(te, h);
+	    rc = (te->fi != NULL);
+	} else {
+	    rc = 1;
 	}
-
-	/* This can fail if we get a different, bad header from callback */
-	rc = (te->fi != NULL);
 	
 	rpmteSetHeader(te, h);
 	headerFree(h);