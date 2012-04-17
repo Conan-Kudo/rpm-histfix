rpm-software-management/rpm

commit 1845c1d711b88723e596c88e8a7730a360029089

Prepare for killing the fsm: Move code for erasing files from FSM_COMMIT to FSM_ERASE

lib/fsm.c
@@ -1779,10 +1779,50 @@ static int fsmStage(FSM_t fsm, fileStage stage)
 		break;
 	    }
 
-            if (!fsm->postpone) {
-                /* Rename/erase item. */
-		rc = fsmNext(fsm, FSM_COMMIT);
-            }
+            /* Remove erased files. */
+	    if (!fsm->postpone && fsm->action == FA_ERASE) {
+		rpmte te = fsmGetTe(fsm);
+		if (S_ISDIR(st->st_mode)) {
+		    rc = fsmRmdir(fsm->path);
+		    if (!rc) break;
+		    switch (rc) {
+		    case CPIOERR_ENOENT: /* XXX rmdir("/") linux 2.2.x kernel hack */
+		    case CPIOERR_ENOTEMPTY:
+	/* XXX make sure that build side permits %missingok on directories. */
+			if (fsm->fflags & RPMFILE_MISSINGOK)
+			    break;
+
+			/* XXX common error message. */
+			rpmlog(
+			    (strict_erasures ? RPMLOG_ERR : RPMLOG_DEBUG),
+			    _("%s rmdir of %s failed: Directory not empty\n"),
+				rpmteTypeString(te), fsm->path);
+			break;
+		    default:
+			rpmlog(
+			    (strict_erasures ? RPMLOG_ERR : RPMLOG_DEBUG),
+				_("%s rmdir of %s failed: %s\n"),
+				rpmteTypeString(te), fsm->path, strerror(errno));
+			break;
+		    }
+		} else {
+		    rc = fsmUnlink(fsm->path, fsm->mapFlags);
+		    if (!rc) break;
+		    switch (rc) {
+		    case CPIOERR_ENOENT:
+			if (fsm->fflags & RPMFILE_MISSINGOK)
+			    break;
+		    default:
+			rpmlog(
+			    (strict_erasures ? RPMLOG_ERR : RPMLOG_DEBUG),
+				_("%s unlink of %s failed: %s\n"),
+				rpmteTypeString(te), fsm->path, strerror(errno));
+			break;
+		    }
+		}
+	    }
+	    /* XXX Failure to remove is not (yet) cause for failure. */
+	    if (!strict_erasures) rc = 0;
 
             if (rc) break;
 
@@ -1943,54 +1983,6 @@ static int fsmStage(FSM_t fsm, fileStage stage)
 	    }
 	    free(path);
 	    free(opath);
-	}
-
-	/* Remove erased files. */
-	if (fsm->goal == FSM_PKGERASE) {
-	    if (fsm->action == FA_ERASE) {
-		rpmte te = fsmGetTe(fsm);
-		if (S_ISDIR(st->st_mode)) {
-		    rc = fsmRmdir(fsm->path);
-		    if (!rc) break;
-		    switch (rc) {
-		    case CPIOERR_ENOENT: /* XXX rmdir("/") linux 2.2.x kernel hack */
-		    case CPIOERR_ENOTEMPTY:
-	/* XXX make sure that build side permits %missingok on directories. */
-			if (fsm->fflags & RPMFILE_MISSINGOK)
-			    break;
-
-			/* XXX common error message. */
-			rpmlog(
-			    (strict_erasures ? RPMLOG_ERR : RPMLOG_DEBUG),
-			    _("%s rmdir of %s failed: Directory not empty\n"), 
-				rpmteTypeString(te), fsm->path);
-			break;
-		    default:
-			rpmlog(
-			    (strict_erasures ? RPMLOG_ERR : RPMLOG_DEBUG),
-				_("%s rmdir of %s failed: %s\n"),
-				rpmteTypeString(te), fsm->path, strerror(errno));
-			break;
-		    }
-		} else {
-		    rc = fsmUnlink(fsm->path, fsm->mapFlags);
-		    if (!rc) break;
-		    switch (rc) {
-		    case CPIOERR_ENOENT:
-			if (fsm->fflags & RPMFILE_MISSINGOK)
-			    break;
-		    default:
-			rpmlog(
-			    (strict_erasures ? RPMLOG_ERR : RPMLOG_DEBUG),
-				_("%s unlink of %s failed: %s\n"),
-				rpmteTypeString(te), fsm->path, strerror(errno));
-			break;
-		    }
-		}
-	    }
-	    /* XXX Failure to remove is not (yet) cause for failure. */
-	    if (!strict_erasures) rc = 0;
-	    break;
 	}
 
 	/* XXX Special case /dev/log, which shouldn't be packaged anyways */

Conan-Kudo/rpm-histfix

commit 39f13115dbb6431eb6e7ff01377866c7bb3b1f6d

Minimally fix file/directory removal
- In their previous life these breaks applied to a switch-case but
  now they end up aborting the loop on first successful removal, causing
  everything but the first file/directory of a package to be left behind
  on erase. Fixes the previously unnoticed regression from commit
  1845c1d711b88723e596c88e8a7730a360029089.

lib/fsm.c
@@ -1963,7 +1963,7 @@ int rpmPackageFilesRemove(rpmts ts, rpmte te, rpmfi fi,
             rpmte te = fsmGetTe(fsm);
             if (S_ISDIR(fsm->sb.st_mode)) {
                 rc = fsmRmdir(fsm->path);
-                if (!rc) break;
+                if (!rc) continue;
                 switch (rc) {
                 case CPIOERR_ENOENT: /* XXX rmdir("/") linux 2.2.x kernel hack */
                 case CPIOERR_ENOTEMPTY:
@@ -1986,7 +1986,7 @@ int rpmPackageFilesRemove(rpmts ts, rpmte te, rpmfi fi,
                 }
             } else {
                 rc = fsmUnlink(fsm->path, fsm->mapFlags);
-                if (!rc) break;
+                if (!rc) continue;
                 switch (rc) {
                 case CPIOERR_ENOENT:
                     if (fsm->fflags & RPMFILE_MISSINGOK)