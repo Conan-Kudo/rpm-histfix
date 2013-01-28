rpm-software-management/rpm

commit f7f5f88f9f3d6587e747b034ccb64a3f00ff4e1e

Only create the first instance of a file shared between packages
- When packages share an identical file, we only need to create the
  first one we encounter and skip it for the rest (reverse already
  happens, and must happen, on erase). This has the benefit of
  getting the timestamp to match the first file, which often is
  also the last one to get removed. Also when lots of shared files
  are involved (such as in multilib installations), this can give
  several percents worth of speed gain as we dont unnecessarily rewrite
  the same contents over and over.

lib/transaction.c
@@ -500,6 +500,12 @@ assert(otherFi != NULL);
 				    rpmteNEVRA(otherTe), fn, 0);
 		    free(fn);
 		}
+	    } else {
+		/* Skip create on all but the first instance of a shared file */
+		if (rpmfsGetAction(otherFs, otherFileNum) != FA_UNKNOWN) {
+		    rpmfsSetAction(fs, i, FA_SKIP);
+		    done = 1;
+		}
 	    }
 
 	    /* Try to get the disk accounting correct even if a conflict. */

Conan-Kudo/rpm-histfix

commit 7e078387fd3ffaac23be8d9f3bac977023923208

Be more careful about skipping shared file/directory creation
- Only skip shared file/dir creation if its actually being created
  by another package. Previously we could've decided to skip an entry
  where the other file was also being skipped for some other reason.
- Ensure %ghost entries always have FA_SKIP on install, previously
  they often were FA_CREATE which makes no sense for %ghost. We dont
  encounter %ghosts at all during install in the FSM as they dont
  exist in the payload, but the file disposition calculations need
  to take them into account now that we're avoiding redundant creates.
- Fixes a regression from commit f7f5f88f9f3d6587e747b034ccb64a3f00ff4e1e
  which unearthed the %ghost FA_CREATE issue, reported here:
  http://lists.fedoraproject.org/pipermail/buildsys/2013-January/004047.html

lib/transaction.c
@@ -572,7 +572,8 @@ assert(otherFi != NULL);
 		}
 	    } else {
 		/* Skip create on all but the first instance of a shared file */
-		if (rpmfsGetAction(otherFs, otherFileNum) != FA_UNKNOWN)
+		rpmFileAction oaction = rpmfsGetAction(otherFs, otherFileNum);
+		if (oaction != FA_UNKNOWN && !XFA_SKIPPING(oaction))
 		    rpmfsSetAction(fs, i, FA_SKIP);
 	    }
 
@@ -759,6 +760,17 @@ static void skipInstallFiles(const rpmts ts, rpmte p)
 
 	ix = rpmfiDX(fi);
 	drc[ix]++;
+
+	/*
+	 * Always skip %ghosts on install.
+	 * XXX: Should we skip directory creation if there are only
+	 * %ghosts in it? Traditionally we create the (empty) directory, so
+	 * preserving that behavior for now at least: leave the refcount alone.
+	 */
+	if (rpmfiFFlags(fi) & RPMFILE_GHOST) {
+	    rpmfsSetAction(fs, i, FA_SKIP);
+	    continue;
+	}
 
 	/* Don't bother with skipped files */
 	if (XFA_SKIPPING(rpmfsGetAction(fs, i))) {