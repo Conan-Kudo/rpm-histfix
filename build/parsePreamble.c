rpm-software-management/rpm

commit 9929459f9967ecfe0a05d557bc42531944faa70a

Optimize lookupPackage() a bit
- Remember the name of sub-packages in the Package struct, avoiding
  the need for headerGetString() in a loop, and also avoid unnecessary
  strdup() in the case name is not a partial one.
- Somewhat hysterically, this more than halves the time to parse the
  infamous texlive.spec on my laptop: the parse time is down from
  10.2s to 4.9s.

build/parsePreamble.c
@@ -973,8 +973,6 @@ int parsePreamble(rpmSpec spec, int initialPackage)
     char *NVR = NULL;
     char lang[BUFSIZ];
 
-    pkg = newPackage(&spec->packages);
-	
     if (! initialPackage) {
 	/* There is one option to %package: <pkg> or -n <pkg> */
 	if (parseSimplePart(spec->line, &name, &flag)) {
@@ -996,10 +994,13 @@ int parsePreamble(rpmSpec spec, int initialPackage)
 	} else
 	    NVR = xstrdup(name);
 	free(name);
+	pkg = newPackage(NVR, &spec->packages);
 	headerPutString(pkg->header, RPMTAG_NAME, NVR);
     } else {
 	NVR = xstrdup("(main package)");
-	spec->sourcePackage = newPackage(NULL);
+	pkg = newPackage(NULL, &spec->packages);
+	spec->sourcePackage = newPackage(NULL, NULL);
+	
     }
 
     if ((rc = readLine(spec, STRIP_TRAILINGSPACE | STRIP_COMMENTS)) > 0) {

build/rpmbuild_internal.h
@@ -85,6 +85,7 @@ struct rpmSpec_s {
  * The structure used to store values for a package.
  */
 struct Package_s {
+    char * name;
     Header header;
     rpmds ds;			/*!< Requires: N = EVR */
     rpmds requires;
@@ -323,11 +324,12 @@ rpmRC lookupPackage(rpmSpec spec, const char * name, int flag,
 
 /** \ingroup rpmbuild
  * Create and initialize package control structure.
+ * @param name		package name for sub-packages (or NULL)
  * @param pkglist	package list pointer to append to (or NULL)
  * @return		package control structure
  */
 RPM_GNUC_INTERNAL
-Package newPackage(Package * pkglist);
+Package newPackage(const char *name, Package * pkglist);
 
 /** \ingroup rpmbuild
  * Post-build processing for binary package(s).

build/spec.c
@@ -60,7 +60,6 @@ struct Source * freeSources(struct Source * s)
 
 rpmRC lookupPackage(rpmSpec spec, const char *name, int flag,Package *pkg)
 {
-    const char *pname;
     char *fullName = NULL;
     Package p;
 
@@ -71,29 +70,28 @@ rpmRC lookupPackage(rpmSpec spec, const char *name, int flag,Package *pkg)
 	return RPMRC_OK;
     }
 
-    /* Construct package name */
+    /* Construct partial package name */
     if (flag == PART_SUBNAME) {
-	pname = headerGetString(spec->packages->header, RPMTAG_NAME);
-	rasprintf(&fullName, "%s-%s", pname, name);
-    } else {
-	fullName = xstrdup(name);
+	rasprintf(&fullName, "%s-%s",
+		 headerGetString(spec->packages->header, RPMTAG_NAME), name);
+	name = fullName;
     }
 
-    /* Locate package with fullName */
+    /* Locate package the name */
     for (p = spec->packages; p != NULL; p = p->next) {
-	pname = headerGetString(p->header, RPMTAG_NAME);
-	if (pname && (rstreq(fullName, pname))) {
+	if (p->name && (rstreq(name, p->name))) {
 	    break;
 	}
     }
-    free(fullName);
+    if (fullName != name)
+	free(fullName);
 
     if (pkg)
 	*pkg = p;
     return ((p == NULL) ? RPMRC_FAIL : RPMRC_OK);
 }
 
-Package newPackage(Package *pkglist)
+Package newPackage(const char *name, Package *pkglist)
 {
     Package p = xcalloc(1, sizeof(*p));
     p->header = headerNew();
@@ -102,6 +100,9 @@ Package newPackage(Package *pkglist)
     p->fileList = NULL;
     p->fileFile = NULL;
     p->policyList = NULL;
+
+    if (name)
+	p->name = xstrdup(name);
 
     if (pkglist) {
 	if (*pkglist == NULL) {
@@ -145,6 +146,7 @@ static Package freePackage(Package pkg)
     pkg->icon = freeSources(pkg->icon);
     pkg->triggerFiles = freeTriggerFiles(pkg->triggerFiles);
 
+    free(pkg->name);
     free(pkg);
     return NULL;
 }

Conan-Kudo/rpm-histfix

commit 3ddf676864017f2e2a406f39101bd5a22d74c4b0

Fix regression on addressing main package by its name (RhBug:972994)
- The main package name is not known when we start parsing the preamble,
  so newPackage() is called with NULL name and the early callers use
  NULL to find it. Populate the name as soon as we know it so
  various "%foo -n <name>" operations work on the main package too,
  which got broken by commit 9929459f9967ecfe0a05d557bc42531944faa70a.

build/parsePreamble.c
@@ -670,6 +670,9 @@ static rpmRC handlePreambleTag(rpmSpec spec, Package pkg, rpmTagVal tag,
 	if (rpmCharCheck(spec, field, strlen(field), ".-_+%{}"))
 	   goto exit;
 	headerPutString(pkg->header, tag, field);
+	/* Main pkg name is unknown at the start, populate as soon as we can */
+	if (pkg == spec->packages)
+	    pkg->name = rpmstrPoolId(spec->pool, field, 1);
 	break;
     case RPMTAG_VERSION:
     case RPMTAG_RELEASE: