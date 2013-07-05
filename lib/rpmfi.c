rpm-software-management/rpm

commit 5d3018c4ed476b1b7ac18e2573af517f872cb303

Dont bother with rpmRelocateFileList() if relocations have been specified
- rpmRelocateFileList() doesn't modify anything when no relocations
  are to be done, but what it does is not exactly free, unnecessarily
  calling it is dumb.

lib/rpmte.c
@@ -94,7 +94,7 @@ static rpmfi getFI(rpmte p, Header h)
 				      (RPMFI_NOHEADER | RPMFI_FLAGS_ERASE);
 
     /* relocate stuff in header if necessary */
-    if (rpmteType(p) == TR_ADDED && rpmfsFC(p->fs) > 0) {
+    if (rpmteType(p) == TR_ADDED && rpmfsFC(p->fs) > 0 && p->nrelocs) {
 	if (!headerIsSource(h) && !headerIsEntry(h, RPMTAG_ORIGBASENAMES)) {
 	    rpmRelocateFileList(p->relocs, p->nrelocs, p->fs, h);
 	}

Conan-Kudo/rpm-histfix

commit 1996eb5c3ce102cf2bb2c990a77e46c9d4e0bdaf

Ensure relocatable packages always get install-prefix(es) set (RhBug:979443)
- Scriptlets from relocatable packages should always run with
  $RPM_INSTALL_PREFIX* defined, whether actually being relocated or not.
- Fixes regression introduced by the optimization in commit
  5d3018c4ed476b1b7ac18e2573af517f872cb303. We always need to call
  addPrefixes() but return early from rpmRelocateFileList() when
  no relocations are taking place, fixing the performance regression
  introduced all the way back in cb8241dda783f7e8c143b08fecf57fe89a39c3a6
  which is what 5d3018c4ed476b1b7ac18e2573af517f872cb303 was trying
  to fix. Pooh :)

lib/rpmfi.c
@@ -795,7 +795,8 @@ static int addPrefixes(Header h, rpmRelocation *relocations, int numRelocations)
 	headerPutStringArray(h, RPMTAG_INSTPREFIXES, actualRelocations, numActual);
     }
     free(actualRelocations);
-    return numActual;
+    /* When any relocations are present there'll be more work to do */
+    return 1;
 }
 
 static void saveRelocs(Header h, rpmtd bnames, rpmtd dnames, rpmtd dindexes)
@@ -836,7 +837,8 @@ void rpmRelocateFileList(rpmRelocation *relocations, int numRelocations,
     int i, j;
     struct rpmtd_s bnames, dnames, dindexes, fmodes;
 
-    addPrefixes(h, relocations, numRelocations);
+    if (!addPrefixes(h, relocations, numRelocations))
+	return;
 
     if (rpmIsDebug()) {
 	rpmlog(RPMLOG_DEBUG, "========== relocations\n");

lib/rpmte.c
@@ -98,7 +98,7 @@ static rpmfi getFI(rpmte p, Header h)
 				      (RPMFI_NOHEADER | RPMFI_FLAGS_ERASE);
 
     /* relocate stuff in header if necessary */
-    if (rpmteType(p) == TR_ADDED && rpmfsFC(p->fs) > 0 && p->nrelocs) {
+    if (rpmteType(p) == TR_ADDED && rpmfsFC(p->fs) > 0) {
 	if (!headerIsSource(h) && !headerIsEntry(h, RPMTAG_ORIGBASENAMES)) {
 	    rpmRelocateFileList(p->relocs, p->nrelocs, p->fs, h);
 	}