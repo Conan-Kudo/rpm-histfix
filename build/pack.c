rpm-software-management/rpm

commit 0bda2faa4de368a87f85084856a5fed701774acb

Populate manual dependencies into spec pkg headers early too
- Similar to commit 1b41c91431d37295701281ff208f99a51f660c89, rpmspec
  and other tools expect to find manually specified dependencies
  from the headers of a freshly parsed spec. This means we need to
  add this cruft two times: once for the manual dependencies and then
  scratch all that and redo from start after automatic dependencies
  have been discovered at the end of package build.
- Fixes another regression (rpmspec dependency queries went dead)
  introduced in commit a357c99c58a5e1367160dfa692f26d14bd3a3df1

build/pack.c
@@ -618,6 +618,11 @@ rpmRC packageBinaries(rpmSpec spec, const char *cookie, int cheating)
 	headerPutUint32(pkg->header, RPMTAG_BUILDTIME, getBuildTime(), 1);
 
 	for (int i=0; i<PACKAGE_NUM_DEPS; i++) {
+	    /* Nuke any previously added dependencies from the header */
+	    headerDel(pkg->header, rpmdsTagN(pkg->dependencies[i]));
+	    headerDel(pkg->header, rpmdsTagEVR(pkg->dependencies[i]));
+	    headerDel(pkg->header, rpmdsTagF(pkg->dependencies[i]));
+	    /* ...and add again, now with automatic dependencies included */
 	    rpmdsPutToHeader(pkg->dependencies[i], pkg->header);
 	}
 

build/parseSpec.c
@@ -548,6 +548,11 @@ static void addTargets(Package Pkgs)
 	headerPutString(pkg->header, RPMTAG_PLATFORM, platform);
 	headerPutString(pkg->header, RPMTAG_OPTFLAGS, optflags);
 
+	/* Add manual dependencies early for rpmspec etc to look at */
+	for (int i=0; i<PACKAGE_NUM_DEPS; i++) {
+	    rpmdsPutToHeader(pkg->dependencies[i], pkg->header);
+	}
+
 	pkg->ds = rpmdsThis(pkg->header, RPMTAG_REQUIRENAME, RPMSENSE_EQUAL);
 	addPackageProvides(pkg);
     }

Conan-Kudo/rpm-histfix

commit 4066a3e9e10f66ebf00b015ce9c08f9f39c0aeda

Resurrect rpmlib() dependencies on source rpms
- Commit 0bda2faa4de368a87f85084856a5fed701774acb caused a regression
  where rpmlib() dependencies are no longer added to src.rpm packages
  as the header is populated early, whereas rpmlib() dependencies
  get added late in the game. So nothing was pushing the rpmlib
  stuff to header. Sigh.

build/pack.c
@@ -708,6 +708,16 @@ rpmRC packageSources(rpmSpec spec, char **cookie)
     headerPutString(sourcePkg->header, RPMTAG_BUILDHOST, buildHost());
     headerPutUint32(sourcePkg->header, RPMTAG_BUILDTIME, getBuildTime(), 1);
 
+    for (int i=0; i<PACKAGE_NUM_DEPS; i++) {
+	/* Nuke any previously added dependencies from the header */
+	headerDel(sourcePkg->header, rpmdsTagN(sourcePkg->dependencies[i]));
+	headerDel(sourcePkg->header, rpmdsTagEVR(sourcePkg->dependencies[i]));
+	headerDel(sourcePkg->header, rpmdsTagF(sourcePkg->dependencies[i]));
+	headerDel(sourcePkg->header, rpmdsTagTi(sourcePkg->dependencies[i]));
+	/* ...and add again, now with automatic dependencies included */
+	rpmdsPutToHeader(sourcePkg->dependencies[i], sourcePkg->header);
+    }
+
     /* XXX this should be %_srpmdir */
     {	char *fn = rpmGetPath("%{_srcrpmdir}/", spec->sourceRpmName,NULL);
 	char *pkgcheck = rpmExpand("%{?_build_pkgcheck_srpm} ", fn, NULL);