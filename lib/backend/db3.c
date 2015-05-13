rpm-software-management/rpm

commit cc924b59cae3d7c473faeee22d68eb6bc914c0c2

Move prefix search implementation into db backend code
This is done so that different backends can implement prefix
search in different ways. The commit also renames DBC_RANGE_SEARCH
to DBC_PREFIX_SEARCH.

lib/backend/db3.c
@@ -852,17 +852,17 @@ rpmRC dbcCursorGet(dbiCursor dbc, const char *keyp, size_t keylen,
 	if (keyp) {
 	    key.data = (void *) keyp; /* discards const */
 	    key.size = keylen;
-
-	    if (searchType == DBC_RANGE_SEARCH)
-		cflags = DB_SET_RANGE;
-	    else
-		cflags = DB_SET;
+	    cflags = searchType == DBC_PREFIX_SEARCH ? DB_SET_RANGE : DB_SET;
 	}
 
-	dbrc = dbiCursorGet(dbc, &key, &data, cflags);
-
-	if (dbrc == 0) {
+	for (;;) {
 	    dbiIndexSet newset = NULL;
+	    dbrc = dbiCursorGet(dbc, &key, &data, cflags);
+	    if (dbrc != 0)
+		break;
+	    if (searchType == DBC_PREFIX_SEARCH &&
+		    (key.size < keylen || memcmp(key.data, keyp, keylen) != 0))
+		break;
 	    dbt2set(dbi, &data, &newset);
 	    if (*set == NULL) {
 		*set = newset;
@@ -870,6 +870,22 @@ rpmRC dbcCursorGet(dbiCursor dbc, const char *keyp, size_t keylen,
 		dbiIndexSetAppendSet(*set, newset, 0);
 		dbiIndexSetFree(newset);
 	    }
+	    if (searchType != DBC_PREFIX_SEARCH)
+		break;
+	    key.data = NULL;
+	    key.size = 0;
+	    cflags = DB_NEXT;
+	}
+
+	/* fixup result status for prefix search */
+	if (searchType == DBC_PREFIX_SEARCH) {
+	    if (dbrc == DB_NOTFOUND && *set != NULL && (*set)->count > 0)
+		dbrc = 0;
+	    else if (dbrc == 0 && (*set)->count == 0)
+		dbrc = DB_NOTFOUND;
+	}
+
+	if (dbrc == 0) {
 	    rc = RPMRC_OK;
 	} else if (dbrc == DB_NOTFOUND) {
 	    rc = RPMRC_NOTFOUND;

lib/backend/dbi.h
@@ -73,7 +73,7 @@ enum dbcFlags_e {
 
 enum dbcSearchType_e {
     DBC_NORMAL_SEARCH   = 0,
-    DBC_RANGE_SEARCH    = (1 << 0),
+    DBC_PREFIX_SEARCH   = (1 << 0),
 };
 
 /** \ingroup dbi

lib/rpmdb.c
@@ -247,11 +247,8 @@ static rpmRC indexPrefixGet(dbiIndex dbi, const char *pfx, size_t plen,
 			    dbiIndexSet *set)
 {
     rpmRC rc = RPMRC_FAIL; /* assume failure */
-    unsigned int keylen;
-    const void *key;
 
-
-    if (dbi != NULL) {
+    if (dbi != NULL && pfx) {
 	dbiCursor dbc = dbiCursorInit(dbi, DBC_READ);
 
 	if (plen == 0) {
@@ -259,37 +256,7 @@ static rpmRC indexPrefixGet(dbiIndex dbi, const char *pfx, size_t plen,
 	    if (plen == 0)
 		plen++; /* XXX "/" fixup */
 	}
-	/*
-	 * Start searching at  position of the first db key greater than or
-	 * equal to pfx. All db keys which starts with pfx must be located
-	 * at this or at following positions.
-	 */
-	rc = dbcCursorGet(dbc, pfx, plen, set, DBC_RANGE_SEARCH);
-
-	while (1) {
-	    if (rc != RPMRC_OK)
-		break;
-
-	    key = dbiCursorKey(dbc, &keylen);
-
-	    if (keylen < plen) {
-		(*set)->count--;
-		break;
-	    }
-
-	    if (memcmp(key, pfx, plen)) {
-		(*set)->count--;
-		break;
-	    }
-
-	    rc = dbcCursorGet(dbc, NULL, 0, set, DBC_NORMAL_SEARCH);
-	}
-
-	if (rc == RPMRC_NOTFOUND && *set && (*set)->count > 0)
-	    rc = RPMRC_OK;
-
-	if (rc == RPMRC_OK && (*set)->count == 0)
-	    rc = RPMRC_NOTFOUND;
+	rc = dbcCursorGet(dbc, pfx, plen, set, DBC_PREFIX_SEARCH);
 
 	dbiCursorFree(dbc);
     }

Conan-Kudo/rpm-histfix

commit 626f9572e3eb1830bc7f4db4c6c1519b8b3fba1f

Fix segfault in prefix search implementation
- Regression from cc924b59cae3d7c473faeee22d68eb6bc914c0c2

lib/backend/db3.c
@@ -1052,7 +1052,7 @@ rpmRC idxdbGet(dbiIndex dbi, dbiCursor dbc, const char *keyp, size_t keylen,
 	if (searchType == DBC_PREFIX_SEARCH) {
 	    if (dbrc == DB_NOTFOUND && *set != NULL && (*set)->count > 0)
 		dbrc = 0;
-	    else if (dbrc == 0 && (*set)->count == 0)
+	    else if (dbrc == 0 && (*set == NULL || (*set)->count == 0))
 		dbrc = DB_NOTFOUND;
 	}
 