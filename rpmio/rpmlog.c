rpm-software-management/rpm

commit 11b005c957fb0e52d42078480104d3e27e95e609

Try to print error on stderr if log function failed (rhbz:#1139444)

rpmio/rpmlog.c
@@ -190,10 +190,14 @@ static int rpmlogDefault(FILE *stdlog, rpmlogRec rec)
         break;
     }
 
-    (void) fputs(rpmlogLevelPrefix(rec->pri), msgout);
+    if (fputs(rpmlogLevelPrefix(rec->pri), msgout) == EOF)
+	perror("Error occurred during writing of a log message");
 
-    (void) fputs(rec->message, msgout);
-    (void) fflush(msgout);
+    if (fputs(rec->message, msgout) == EOF)
+	perror("Error occurred during writing of a log message");
+
+    if (fflush(msgout) == EOF)
+	perror("Error occurred during writing of a log message");
 
     return (rec->pri <= RPMLOG_CRIT ? RPMLOG_EXIT : 0);
 }

Conan-Kudo/rpm-histfix

commit 852f2a559a08d07a47114e91de0f2fbf5961d854

Don't show error message if log function fails because of broken pipe.
- regression from commit 11b005c957fb0e52d42078480104d3e27e95e609
- rhbz: #1231138

rpmio/rpmlog.c
@@ -6,6 +6,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <errno.h>
 #include <rpm/rpmlog.h>
 #include "debug.h"
 
@@ -190,13 +191,13 @@ static int rpmlogDefault(FILE *stdlog, rpmlogRec rec)
         break;
     }
 
-    if (fputs(rpmlogLevelPrefix(rec->pri), msgout) == EOF)
+    if (fputs(rpmlogLevelPrefix(rec->pri), msgout) == EOF && errno != EPIPE)
 	perror("Error occurred during writing of a log message");
 
-    if (fputs(rec->message, msgout) == EOF)
+    if (fputs(rec->message, msgout) == EOF && errno != EPIPE)
 	perror("Error occurred during writing of a log message");
 
-    if (fflush(msgout) == EOF)
+    if (fflush(msgout) == EOF && errno != EPIPE)
 	perror("Error occurred during writing of a log message");
 
     return (rec->pri <= RPMLOG_CRIT ? RPMLOG_EXIT : 0);